30e16769018ec0814c5ad5878a434def15cf7ffd
artursm <makarov.artur9090@gmail.com>
2022-10-20 16:37:41 +0300

    Tuples and Maps

---
+++ src/main/scala/lectures/part3fp/TuplesAndMaps.scala
@@ -0,0 +1,43 @@
+package lectures.part3fp
+
+object TuplesAndMaps extends App {
+
+  // tuples = finite ordered "lists"
+  val aTuple = (2, "Hello, Scala") // Tuple2[Int, String] = (Int, String)
+
+  println(aTuple._1)  // 2
+  println(aTuple.copy(_2 = "goodbye Java"))
+  println(aTuple.swap) // ("hello, Scala", 2)
+
+  // Maps - keys -> values
+  val aMap: Map[String, Int] = Map()
+
+  val phonebook = Map(("Jim", 555), "Daniel" -> 789).withDefaultValue(-1)  // sintactic sugar
+  // a -> b is sugar for (a, b)
+  println(phonebook)
+
+  // map ops
+  println(phonebook.contains("Jim"))
+  println(phonebook("Mary"))
+
+  // add a pairing
+  val newPairing = "Mary" -> 678
+  val newPhonebook = phonebook + newPairing
+  println(newPhonebook)
+
+  // functionals on maps
+  // map, fltaMap, filter
+
+  println(phonebook.map(pair => pair._1.toLowerCase -> pair._2))
+
+  // filterKeys
+  println(phonebook.view.filterKeys(x => x.startsWith("J")).toMap)
+  // mapValues
+  println(phonebook.view.mapValues(number => "0245-" + number).toMap)
+
+  // conversions to other collections
+  println(phonebook.toList)
+  println(List(("Daniel", 555)).toMap)
+  val names = List("Bob", "James", "Angela", "Mary", "Daniel", "Jim")
+  println(names.groupBy(name => name.charAt(0)))
+}
